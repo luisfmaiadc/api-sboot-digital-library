4c34dcc5da69179b5495f4401c06cc6082ab005b
luisfmaiadc <lf.mcosta23@gmail.com>
2024-11-07 18:58:47 -0300

    Inicializando projeto de API REST livraria digital.

---
+++ src/main/java/com/portfolio/api_sboot_digital_library/ApiSbootDigitalLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.api_sboot_digital_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSbootDigitalLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSbootDigitalLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/api_sboot_digital_library/ApiSbootDigitalLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.api_sboot_digital_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSbootDigitalLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
